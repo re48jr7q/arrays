31efbd9cd1cf587143096c8817e7b5aac2acd3a3
re48jr7q <erwin.degeus45@gmail.com>
2022-08-24 16:59:21 +0200

    Add project files.

---
+++ arrays.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+
+int main()
+{
+    int testScore[30]{};
+}
---
+++ arrays.h
@@ -0,0 +1,14 @@
+#ifndef ARRAYS
+#define ARRAYS
+
+namespace Students
+{
+	int testScoreStudent1{};
+	int testScoreStudent2{};
+	int testScoreStudent3{};
+	int testScoreStudent4{};
+	//...
+	int testScoreStudent30{};
+}
+
+#endif
